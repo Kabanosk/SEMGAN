157ccfe201eaa29b44a2466d43f375f33d4a8937
kabanosk <56364007+Kabanosk@users.noreply.github.com>
2024-12-06 22:00:14 +0100

    Add simple train config and update load_config function

--- src/config/config.py
+++ src/config/config.py
@@ -1,9 +1,27 @@
+import sys
 from pathlib import Path
 
 import yaml
 
 
 def load_config(path: str | Path) -> dict:
-    with open(path) as file:
-        config = yaml.safe_load(file)
-    return config
+    """Function for loading configuration file.
+
+    Args:
+        path: Path to configuration file.
+
+    Returns:
+        dict: Dictionary object with values from configuration file.
+    """
+    try:
+        with open(path) as file:
+            config = yaml.safe_load(file)
+        return config
+
+    except (FileNotFoundError, PermissionError) as err:
+        print(f"Error related to file of {path = } occurred: {repr(err)}.")
+        sys.exit(0)
+
+    except yaml.YAMLError as err:
+        print(f"Error related to yaml file of {path = } occurred: {repr(err)}.")
+        sys.exit(0)
