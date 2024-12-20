af80ea608b6496ff41627e299f74a982144b5021
kabanosk <56364007+Kabanosk@users.noreply.github.com>
2024-12-20 19:44:10 +0100

    Update configuration files

--- src/config/config.py
+++ src/config/config.py
@@ -1,27 +1,43 @@
-import sys
+import logging
 from pathlib import Path
-
 import yaml
+from src.utils.logger import create_logger
 
 
 def load_config(path: str | Path) -> dict:
     """Function for loading configuration file.
 
     Args:
-        path: Path to configuration file.
+        path: Path to the configuration file.
 
     Returns:
-        dict: Dictionary object with values from configuration file.
+        dict: Dictionary object with values from the configuration file.
     """
+    logger: logging.Logger = create_logger()
+    path = Path(path)
+
+    if not path.exists():
+        logger.error(f"Config file '{path}' not found.")
+        raise ValueError(f"Configuration file '{path}' not found, and no default provided.")
+
     try:
-        with open(path) as file:
+        with path.open() as file:
             config = yaml.safe_load(file)
+
+        if not isinstance(config, dict):
+            raise ValueError("Loaded configuration is not a dictionary.")
+
+        logger.info(f"Config file '{path}' loaded successfully.")
         return config
 
     except (FileNotFoundError, PermissionError) as err:
-        print(f"Error related to file of {path = } occurred: {repr(err)}.")
-        sys.exit(0)
+        logger.error(f"Error opening file '{path}': {err}")
+        raise err
 
     except yaml.YAMLError as err:
-        print(f"Error related to yaml file of {path = } occurred: {repr(err)}.")
-        sys.exit(0)
+        logger.error(f"Error parsing YAML file '{path}': {err}")
+        raise err
+
+    except ValueError as err:
+        logger.error(f"Invalid data format in '{path}': {err}")
+        raise err
